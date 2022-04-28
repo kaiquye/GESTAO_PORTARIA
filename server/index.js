696bbe5d72eb178675091e8745fdc60d78942070
kaiquye <kaiquemendesilva82@gmail.com>
2022-04-28 17:04:25 -0300

    heroku

--- server/index.js
+++ server/index.js
@@ -2,32 +2,28 @@ require('dotenv').config();
 const express = require('express');
 // server para o websocket
 const server = require('http').createServer(express());
-const io = require('../src/models/WebSocket');
-const Cors = require('../src/midlleware/cors');
+const io = require('./src/models/WebSocket');
+const Cors = require('./src/midlleware/cors');
 
 class Server {
     App;
-
     constructor() {
         this.App = express();
         this.middlleware();
         this.routes();
     }
-
     middlleware() {
         this.App.use(Cors.Config());
     }
-
     routes() {
         this.App.get('/teste', (req, res) => {
+            res.send('opa')
             console.log('testando')
         })
     }
-
     StartServer() {
         this.App.listen(process.env.PORT, () => console.log(`start server in http://localhost:${process.env.PORT}/`))
     }
-
 }
 
 (() => {
